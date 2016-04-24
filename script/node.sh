450132fd6bc3e9c7d21490e9b7804507203d2ff2
Gearnode <bfrimin@student.42.fr>
2016-04-24 23:42:58 +0200

    Add NodeJS version install

--- script/node.sh
+++ script/node.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+NODE_VERSION=5.0
+
 if test ! $(which nvm)
 then
   echo "Installing NVM"
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 fi
 
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
