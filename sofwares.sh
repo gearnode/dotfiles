a48749f400c8959db265dcb9043c39eb02e0b5c3
Gearnode <bfrimin@student.42.fr>
2016-04-24 02:39:49 +0200

    Use Brewfile

--- sofwares.sh
+++ sofwares.sh
@@ -2,37 +2,6 @@
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-SOFTWARES=(
-  zsh
-  git
-  vim
-  neovim
-  redis
-  postgresql
-  curl
-  docker
-  jq # JSON parser
-  erlang
-  elixir
-  tmux
-  gcc49
-  tree
-  python3
-  imagemagick
-  wget
-  openssl
-)
-
-APPLICATIONS=(
-  google-chrome
-  google-chrome-canary
-  transmission
-  iterm2
-  vlc
-  slack
-  skype
-)
-
 if test ! $(witch brew)
 then
   echo "Installing Homebrew"
@@ -40,15 +9,6 @@ then
 fi
 
 brew install caskroom/cask/brew-cask
-
-for SOFTWARE in ${SOFTWARES[@]} ; do
-  brew install ${SOFTWARE}
-  brew link ${SOFTWARE} --force
-done
-
-for APPLICATION in ${APPLICATIONS[@]} ; do
-  brew cask install ${APPLICATION}
-done
-
+brew bundle
 brew cleanup
 brew cask cleanup
