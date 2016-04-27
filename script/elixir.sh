62c23fc05bccdded3f833e4b4741c683fbd7a958
Gearnode <bfrimin@student.42.fr>
2016-04-27 21:50:48 +0200

    Style

--- script/elixir.sh
+++ script/elixir.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-if test ! $(which kiex)
-then
+if test ! $(which kiex) ; then
   echo "Installing Kiex"
   \curl -sSL https://raw.githubusercontent.com/taylor/kiex/master/install | bash -s
 fi
