d15d713caff08733f67c285e69f90e4fc8d4f2b9
Bryan FRIMIN <friminb@gmail.com>
2017-07-22 12:44:38 -0700

    Style

--- iex.exs
+++ iex.exs
@@ -3,7 +3,6 @@ IEx.configure(default_prompt: "repl(%counter)>")
 IEx.configure(history_size: 1000)
 IEx.configure(width: 80)
 
-
 debug = fn x ->
   IO.puts "# –––––––––––– DEBUG –––––––––––– #"
   IO.inspect x
